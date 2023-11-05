d4573b61c266db269e759934f54613e29ca54337
Yash-0702 <rockstarmehta2002@gmail.com>
2023-11-06 01:08:09 +0530

    Portfolio website 1

---
+++ src/data.js
@@ -0,0 +1,33 @@
+
+export const products = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/564x/f5/88/fa/f588faebe244b4688c62d66f6521f780.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://lekker.qodeinteractive.com/wp-content/uploads/2020/06/landing-home-img-1-scaled.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/564x/78/ea/89/78ea8987e4a4a386c87ca88f55cb7d4b.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://lekker.qodeinteractive.com/wp-content/uploads/2020/06/landing-home-img-12.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://lekker.qodeinteractive.com/wp-content/uploads/2020/06/landing-home-img-9.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://lekker.qodeinteractive.com/wp-content/uploads/2020/06/landing-home-img-1-scaled.jpg",
+      link: "http://lama.dev",
+    },
+  ];
